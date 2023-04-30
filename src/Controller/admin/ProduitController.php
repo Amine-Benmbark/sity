83919efe30452262bd1fc324bd75c60ef088db9d
amine-benmbark <dortmund91000@hotmail.com>
2023-04-30 19:35:59 +0200

    [ADD] img in bdd and recupere in template

--- src/Controller/admin/ProduitController.php
+++ src/Controller/admin/ProduitController.php
@@ -5,24 +5,43 @@ namespace App\Controller\admin;
 
 use App\Entity\Produit;
 use App\Form\ProduitFormType;
+use App\Service\FileUploader;
+use App\Service\Helpers;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as ControllerAbstractController;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/produit', name:'admin_produit')]
 class ProduitController extends ControllerAbstractController
 {
-    #[Route('/', name:'index')]
-    public function index(): Response
+    private $manager;
+    public function __construct(Helpers $app, ManagerRegistry $doctrine, EntityManagerInterface $entityManager)
     {
-        return $this->render('admin/produit/produit.html.twig');
+        $this->manager = $doctrine->getManager();
+
     }
 
+    #[Route('/', name:'index')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+
+        $produit = $this->manager->getRepository(Produit::class)->findAll();
+
+        return $this->render('admin/produit/produit.html.twig', [
+            'produit' => $produit,
+        ]);
+    }
+
+
+
     #[Route('/ajout', name:'add')]
-    public function add(Request $request, EntityManagerInterface $em): Response
+    public function add(Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
@@ -35,6 +54,32 @@ class ProduitController extends ControllerAbstractController
         if($formproduit->isSubmitted() && $formproduit->isValid()){
             $prix = $produit->getPrix() * 100;
             $produit->setPrix($prix);
+
+            $imgFile = $formproduit->get('img')->getData();
+            if ($imgFile) {
+                $originalFilename = pathinfo($imgFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imgFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $imgFile->move(
+                        $this->getParameter('img_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                    // $imgFile = $formproduit->get('img')->getData();
+                    // if ($imgFile) {
+                    //     $imgFileName = $fileUploader->upload($imgFile);
+                    //     $produit->setimg($imgFileName);
+                    // }
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $produit->setimg($newFilename);
+            }
 
             $em->persist($produit);
             $em->flush();
@@ -50,6 +95,8 @@ class ProduitController extends ControllerAbstractController
         // return $this->$this->renderForm('admin/produit/add.html.twig', compact('formproduit'));
     }
 
+
+    
     #[Route('/edition/{id}', name:'edit')]
     public function edit(Produit $produit, Request $request, EntityManagerInterface $em): Response
     {
